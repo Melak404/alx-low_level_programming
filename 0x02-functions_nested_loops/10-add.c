dbf509d7feaa6e7cec947daf66853e86a6c48a42
Melak404 <117733191+Melak404@users.noreply.github.com>
2022-12-14 17:43:24 +0000

    Task 10 - add two integerswq

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add -  add two integers
+ * @x: First integer
+ * @y: Second integer
+ * Return: returns sum
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+
+	return (sum);
+}
