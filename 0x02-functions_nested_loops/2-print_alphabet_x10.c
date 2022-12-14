fa8c578dda7485cd4a2236113f1597076ef34493
Melak404 <117733191+Melak404@users.noreply.github.com>
2022-12-14 11:57:47 +0000

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints a to z ten times
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int a, n;
+
+	for (n = 1; n < 10; n++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
