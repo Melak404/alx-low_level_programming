0ea271f2271dee175b8ef6f5c3e778ac038d571e
Melak404 <117733191+Melak404@users.noreply.github.com>
2022-12-14 21:11:51 +0000

    Task 14 Fix

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,24 +1,26 @@
 #include<stdio.h>
 
 /**
- * main - Numbers
+ * main - 50 Fibonacci numbers
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int i;
-	int a, b;
+	int i, j = 0, k = 1, l;
 
-	for (i = 0; i < 12; i++)
+	for (i = 0; i < 50; i++)
 	{
-		printf("%d", i);
-		printf(", ");
-
-		a = b;
-		b = i;
-		i = a + b;
+		l = j + k;
+		j = k;
+		k = l;
+		printf("%i", l);
+		if (i == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
 	}
-
 	return (0);
 }
