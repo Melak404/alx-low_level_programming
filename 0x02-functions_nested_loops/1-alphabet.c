38be1cd127c318cf9d277dab9843b6b94ca1a853
Melak404 <117733191+Melak404@users.noreply.github.com>
2022-12-14 11:41:14 +0000

    C - Functions, nested loops Task 0,1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	int a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,7 @@
+## 0x02. C - Functions, nested loops
+### 0. _putchar
+### 2. 10 x alphabet
+### 
+### 
+### 
+###  
