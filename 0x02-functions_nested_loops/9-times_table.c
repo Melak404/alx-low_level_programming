7fa28e88b4d505e081252013e1dea0a910e010aa
Melak404 <117733191+Melak404@users.noreply.github.com>
2022-12-14 17:36:28 +0000

    Hot fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,33 +6,34 @@
  */
 void times_table(void)
 {
-  int r, c, d;
+	int r, c, d;
 
-  for (r = 0; r <= 9; r++)
-  {
-    _putchar('0');
-    _putchar(',');
-    _putchar(' ');
-    for (c = 1; c <= 9; c++)
-    {
-      d = (r * c);
-      if ((d / 10) > 0)
-      {
-        _putchar((d / 10) + '0');
-      }
-      else
-      {
-        _putchar(' ');
-      }
-      _putchar((d % 10) + '0');
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 1; c <= 9; c++)
+		{
+			d = (r * c);
 
-      if (c < 9)
-      {
-        _putchar(',');
-        _putchar(' ');
-      }
-    }
-    _putchar('\n');
-  }
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
 
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+
+		_putchar('\n');
+	}
 }
