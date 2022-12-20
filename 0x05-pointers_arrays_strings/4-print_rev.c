ad4cd6cb3cde5ab2c00d42394370f2f2f310c428
Melak404 <117733191+Melak404@users.noreply.github.com>
2022-12-20 12:34:16 +0000

    Print rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_rev - Print in reverse
+  *
+  * @s: input
+  */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	for (i -= 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
