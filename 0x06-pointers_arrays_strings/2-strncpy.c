e4e42408650102dcbd3576d1412699d69e6927e8
Melak404 <117733191+Melak404@users.noreply.github.com>
2022-12-22 11:27:09 -0800

    Str

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy string
+ * @dest: input
+ * @src: input
+ * @n: input
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	i = i;
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
